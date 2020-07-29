e24f65348a051143a1135f8f65512f44bb0c49f0
florent beauchamp <florent.beauchamp@gmail.com>
2020-07-29 08:31:32 +0200

    wip

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/api/register", name="register", methods={"POST"})
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, SerializerInterface $serializer,  ValidatorInterface $validator)
+    {
+        $username = $request->get('username');
+        $password = $request->get('password');
+        if(isset($username, $password)) {
+            $user = new User();
+            $user->setUsername($username);
+            $user->setPassword($passwordEncoder->encodePassword($user, $password));
+            $user->setRoles(['ROLE_ADMIN']);
+            $errors = $validator->validate($user);
+            if(count($errors)) {
+                $errors = $serializer->serialize($errors, 'json');
+                return new Response($errors, 500, [
+                    'Content-Type' => 'application/json'
+                ]);
+            }
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $data = [
+                'status' => 201,
+                'message' => 'L\'utilisateur a été créé'
+            ];
+
+            return new JsonResponse($data, 201);
+        }
+        $data = [
+            'status' => 500,
+            'message' => 'Vous devez renseigner les clés username et password'
+        ];
+        return new JsonResponse($data, 500);
+    }
+
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login(Request $request)
+    {
+        $user = $this->getUser();
+        return $this->json([
+            'username' => $user->getUsername(),
+            'roles' => $user->getRoles()
+        ]);
+    }
+}
